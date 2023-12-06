6bf5b6370a98895333c098d5395f4656dd63c5d5
Subhajyoti Naskar <subhajyoti3845@gmail.com>
2023-12-07 01:02:23 +0530

    Created Intro part

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Navbar from "./components/Navbar/Navbar";
 import './App.css'
+import Intro from "./components/Info/Intro";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Intro />
     </div>
   );
 }
---
+++ src/components/Info/Intro.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+
+import LinkedIn from '../../img/linkedin.png';
+import Github from '../../img/github.png';
+import Instagram from '../../img/instagram.png';
+
+import Paint from '../../img/paint.png';
+import Boy from '../../img/boy.webp';
+import Crown from '../../img/crown.png';
+import GlassesEmoji from '../../img/glassesimoji.png';
+
+import FloatingDiv from '../FloatingDiv/FloatingDiv';
+
+import './Intro.css';
+
+const Intro = () => {
+  return (
+    <div className='intro'>
+      <div className="intro__left">
+        <div className="intro__left--name">
+          <span>Hey! I Am</span>
+          <span>Subhajyoti Naskar</span>
+          <span>Front-end developer transforming ideas into captivating web interfaces with code, creativity, and user-centric design expertise.</span>
+        </div>
+        <button className='button intro__btn'>Hire Me</button>
+        <div className="intro__left--icon">
+          <img src={Github} alt="Github" />
+          <img src={LinkedIn} alt="LinkedIn" />
+          <img src={Instagram} alt="Instagram" />
+        </div>
+      </div>
+      <div className="intro__right">
+        <img src={Paint} alt="Paint" />
+        <img src={Boy} alt="Boy" />
+        <img src={GlassesEmoji} alt="glasses emoji" />
+        <div className='floatingdiv__wrapper floatingdiv__wrapper--crown'>
+          <FloatingDiv image={Crown} text1="Web" text2="Developer" />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Intro;
