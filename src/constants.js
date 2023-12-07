8df52ec4ad6a3bc1d6d3add8819e3fa910284e6b
Subhajyoti Naskar <subhajyoti3845@gmail.com>
2023-12-08 00:13:25 +0530

    Added experience data

---
+++ src/constants.js
@@ -0,0 +1,21 @@
+export const employementDetails = {
+    dateOfJoiningOfFirstJob : '2021-06-16',
+    totalNoOfProjects : '3',
+    workExperience: [
+        {
+            organisation: 'J&F India',
+            startDate: '2022-06-01',
+            stillWorking: true,
+            jobTitle: 'SDE-1',
+            location: 'Noida',
+        },
+        {
+            organisation: 'Nokia Solutions and Networks',
+            startDate: '2021-06-16',
+            lastDate: '2022-05-30',
+            stillWorking: false,
+            jobTitle: 'Graduate Engineer Trainee',
+            location: 'Noida',
+        },
+    ]
+}
