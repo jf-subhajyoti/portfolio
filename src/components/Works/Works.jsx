3db5268e291973a7391364ad09cb36f6bbe0d2d2
Subhajyoti Naskar <subhajyoti3845@gmail.com>
2023-12-10 15:36:39 +0530

    Added right part of work experience

--- src/components/Works/Works.jsx
+++ src/components/Works/Works.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import { employementDetails } from '../../constants';
+
 import './Works.css';
 
 const Works = () => {
@@ -12,7 +14,29 @@ const Works = () => {
                 </div>
                 <button className='button works__btn'>Download CV</button>
             </div>
-            <div className="works__right">I'm the right side</div>
+            <div className="works__right">
+                {
+                    employementDetails.workExperience.map((exp, index) => (
+                        <div key={exp.startDate} className="works__logoWrapper">
+                            <div className="works__logo">
+                                <img src={exp.logo} alt="J&F" />
+                            </div>
+                            <div className="works__details">
+                                <div className="works__details--organisation">
+                                    <span className='title'>Organisation : </span><span className='description'>{exp.organisation}</span>
+                                </div>
+                                <div className='works__details--title'>
+                                    <span className='title'>Job Title : </span><span className='description'>{exp.jobTitle}</span>
+                                </div>
+                                <div className='works__details--duration'>
+                                    <span className='title'>From : </span><span className='description'>{exp.startDate} to {exp.lastDate}</span>
+                                </div>
+                            </div>
+                        </div>
+
+                    ))
+                }
+            </div>
         </div>
     )
 }
