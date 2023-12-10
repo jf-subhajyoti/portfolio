35192b309ee6fa303c3bd8593e6d34c707771396
Subhajyoti Naskar <subhajyoti3845@gmail.com>
2023-12-10 15:35:21 +0530

    Updated employee details

--- src/constants.js
+++ src/constants.js
@@ -1,21 +1,27 @@
+import Nokia from './img/nokia.png';
+import JF from './img/j&f.png';
+
 export const employementDetails = {
     dateOfJoiningOfFirstJob : '2021-06-16',
     totalNoOfProjects : '3',
     workExperience: [
         {
             organisation: 'J&F India',
-            startDate: '2022-06-01',
+            startDate: '2022/0601',
+            lastDate: 'Present',
             stillWorking: true,
             jobTitle: 'SDE-1',
             location: 'Noida',
+            logo: JF,
         },
         {
             organisation: 'Nokia Solutions and Networks',
-            startDate: '2021-06-16',
-            lastDate: '2022-05-30',
+            startDate: '2021/06/16',
+            lastDate: '2022/05/30',
             stillWorking: false,
             jobTitle: 'Graduate Engineer Trainee',
             location: 'Noida',
+            logo: Nokia,
         },
     ]
 }
