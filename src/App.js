e6d987be7a82838a19f1bb08a59a2bfa3160d990
Subhajyoti Naskar <subhajyoti3845@gmail.com>
2023-12-17 21:20:41 +0530

    Created contact ui and structure

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Intro from "./components/Info/Intro";
 import Experience from "./components/Experience/Experience";
 import Works from "./components/Works/Works";
 import Portfolio from "./components/Portfolio/Portfolio";
+import Contacts from "./components/Contacts/Contacts";
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
       <Experience />
       <Works />
       <Portfolio />
+      <Contacts />
     </div>
   );
 }
