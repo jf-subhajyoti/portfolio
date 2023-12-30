09b1c7439183885c4b9e96e2f10a4f12273a319e
Subhajyoti Naskar <subhajyoti3845@gmail.com>
2023-12-30 13:08:56 +0530

    Imporoved ui

--- src/components/Contacts/Contacts.jsx
+++ src/components/Contacts/Contacts.jsx
@@ -7,17 +7,17 @@ const Contacts = () => {
     <div className='contact__wrapper'>
       <div className="contact__wrapper--left">
         <div className="contact__title">
-            <span>Get in touch</span>
-            <span>Contact me</span>
+          <span>Get in touch</span>
+          <span>Contact me</span>
         </div>
-            <div className='blur'></div>
+        <div className='blur'></div>
       </div>
       <div className="contact__wrapper--right">
         <form>
-            <input type="text" name='user_name' className='from__input user' placeholder='Name' />
-            <input type="email" name='user_email' className='from__input email' placeholder='Email' />
-            <textarea name="message" className='from__input from__textarea' placeholder='Message'></textarea>
-            <input type="submit" value="Send" className='button' placeholder='Name' />
+          <input type="text" name='user_name' className='from__input user' placeholder='Name' />
+          <input type="email" name='user_email' className='from__input email' placeholder='Email' />
+          <textarea name="message" className='from__input from__textarea' placeholder='Message'></textarea>
+          <input type="submit" value="Send" className='button' placeholder='Name' />
         </form>
         <div className="blur"></div>
       </div>
