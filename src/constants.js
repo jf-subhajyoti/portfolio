c24b54b938ffc32fcc73202b18616e0d6cfa10e5
Subhajyoti Naskar <subhajyoti3845@gmail.com>
2023-12-30 22:54:26 +0530

    Added image and updated the constant file

--- src/constants.js
+++ src/constants.js
@@ -2,6 +2,9 @@ import Nokia from './img/nokia.png';
 import JF from './img/j&f.png';
 import MovixThumbnail from './img/movix.png';
 import SketchboardThumbnail from './img/sketchboard.png';
+import React from './img/React_logo.png';
+import Redux from './img/redux.png';
+import CSS from './img/css.png';
 
 export const employementDetails = {
     dateOfJoiningOfFirstJob : '2021-06-16',
@@ -32,15 +35,27 @@ export const personalProject = [
     {
         name: 'Sketchboard',
         thumbnail: SketchboardThumbnail,
+        githubLink: 'https://github.com/jf-subhajyoti/sketchboard',
+        websiteLink: 'https://sketchboard-lyart.vercel.app/',
         techstack: {
-            'front-end': ['React', 'Redux', 'HTML', 'SCSS'],
+            'Front End': [
+                {name: 'React', logo: React },
+                {name: 'Redux', logo: Redux },
+                {name: 'CSS', logo: CSS },
+            ],
         }
     },
     {
         name: 'Movix',
         thumbnail: MovixThumbnail,
+        githubLink: 'https://github.com/jf-subhajyoti/q-filx',
+        websiteLink: 'https://q-filx.vercel.app/',
         techstack: {
-            'front-end' : ['React', 'Redux', 'HTML', 'SCSS'],
+            'Front End': [
+                { name: 'React', logo: React },
+                { name: 'Redux', logo: Redux },
+                { name: 'CSS', logo: CSS },
+            ],
         }
     }
 ]
