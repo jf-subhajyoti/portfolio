f70f9b6e26b291b60e522396b5b9c1b03c4a9cc0
Subhajyoti Naskar <subhajyoti3845@gmail.com>
2023-12-30 22:55:53 +0530

    Improved code

--- src/components/Works/Works.jsx
+++ src/components/Works/Works.jsx
@@ -42,7 +42,6 @@ const Works = () => {
                                 </div>
                             </div>
                         </div>
-
                     ))
                 }
             </div>
