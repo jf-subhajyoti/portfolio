60d5c1f4468a6aa7f8003e0193430be1efade4f8
Subhajyoti Naskar <subhajyoti3845@gmail.com>
2024-01-02 22:28:55 +0530

    Added toggle button

---
+++ src/components/Toogle/Toggle.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import './Toggle.css';
+
+import { useThemeContext } from '../../ThemeContext';
+
+import Sun from '@iconscout/react-unicons/icons/uil-sun';
+import Moon from '@iconscout/react-unicons/icons/uil-moon';
+
+const Toggle = () => {
+    const { darkTheme, updateTheme } = useThemeContext();
+
+    return (
+        <div className='toggle'>
+            <div className="t-button" style={darkTheme ? {left: '2px'} : {right : '2px'}} onClick={updateTheme}></div>
+            <Moon className='toggle--icon' />
+            <Sun className='toggle--icon' />
+        </div>
+    )
+}
+
+export default Toggle;
